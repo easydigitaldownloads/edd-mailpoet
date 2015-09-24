feb063ea84d6d250af198bffe2df6fc3858c5056
Pippin Williamson <pippin@pippinsplugins.com>
2015-09-24 10:06:54 -0500

    Adding initial files

---
+++ edd-mailpoet.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - MailPoet (formerly Wysija)
+Plugin URL: http://easydigitaldownloads.com/extensions/wysija
+Description: Add customers to your newsletter lists in MailPoet
+Version: 1.3
+Author: Pippin Williamson
+Author URI: http://pippinsplugins.com
+Contributors: mordauk
+Text Domain: edd_wysija
+Domain Path: languages
+*/
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/* PHP Hack to Get Plugin Headers in the .POT File */
+	$edd_wysija_plugin_header_translate = array(
+		__( 'Easy Digital Downloads - MailPoet', 'edd_wysija' ),
+    	__( 'Include a MailPoet signup option with your Easy Digital Downloads checkout', 'edd_wysija' ),
+    	__( 'Pippin Williamson', 'edd_wysija' ),
+    	__( 'http://easydigitaldownloads.com/extension/wysija/', 'edd_wysija' ),
+    );
+
+define( 'EDD_MAILPOET_PATH', dirname( __FILE__ ) );
+
+/*
+|--------------------------------------------------------------------------
+| LICENSING / UPDATES
+|--------------------------------------------------------------------------
+*/
+if( class_exists( 'EDD_License' ) && is_admin() ) {
+	$eddw_license = new EDD_License( __FILE__, 'Wysija', '1.3', 'Pippin Williamson', 'edd_wysija_license_key' );
+}
+
+if( ! class_exists( 'EDD_Newsletter' ) ) {
+	include( dirname( __FILE__ ) . '/includes/class-edd-newsletter.php' );
+}
+
+if( ! class_exists( 'EDD_MailPoet' ) ) {
+	include( dirname( __FILE__ ) . '/includes/class-edd-mailpoet.php' );
+}
+
+$edd_mp = new EDD_MailPoet( 'mailpoet', 'MailPoet' );
+
+/*
+|--------------------------------------------------------------------------
+| INTERNATIONALIZATION
+|--------------------------------------------------------------------------
+*/
+
+function edd_wysija_textdomain() {
+
+	// Set filter for plugin's languages directory
+	$edd_lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
+	$edd_lang_dir = apply_filters( 'edd_wysija_languages_directory', $edd_lang_dir );
+
+	// Load the translations
+	load_plugin_textdomain( 'edd_wysija', false, $edd_lang_dir );
+}
+add_action( 'plugins_loaded', 'edd_wysija_textdomain' );
---
+++ includes/class-edd-mailpoet.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * EDD Mail Chimp class, extension of the EDD base newsletter classs
+ *
+ * @copyright   Copyright (c) 2013, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       2.0
+*/
+
+class EDD_MailPoet extends EDD_Newsletter {
+
+	/**
+	 * Sets up the checkout label
+	 */
+	public function init() {
+		global $edd_options;
+		if( ! empty( $edd_options['edd_wysija_label'] ) ) {
+			$this->checkout_label = trim( $edd_options['edd_wysija_label'] );
+		} else {
+			$this->checkout_label = __( 'Signup for the newsletter', 'edd_wysija' );
+		}
+
+	}
+
+	/**
+	 * Retrieves the lists from Mail Chimp
+	 */
+	public function get_lists() {
+
+		global $edd_options;
+
+		if( ! class_exists( 'WYSIJA' ) ) {
+			return array();
+		}
+
+		$modelList   = WYSIJA::get( 'list','model' );
+		$wysijaLists = $modelList->get( array( 'name', 'list_id' ), array( 'is_enabled' => 1 ) );
+
+		if( ! empty( $wysijaLists ) ) {
+			foreach( $wysijaLists as $list ) {
+
+				$this->lists[ $list['list_id'] ] = $list['name'];
+			}
+		}
+		return (array) $this->lists;
+	}
+
+	/**
+	 * Registers the plugin settings
+	 */
+	public function settings( $settings ) {
+
+		$mailpoet_settings = array(
+			array(
+				'id' => 'edd_wysija_settings',
+				'name' => '<strong>' . __( 'MailPoet Settings', 'edd_wysija' ) . '</strong>',
+				'desc' => __( 'Configure MailPoet Integration Settings', 'edd_wysija' ),
+				'type' => 'header'
+			),
+			array(
+				'id' => 'edd_wysija_list',
+				'name' => __('Choose a list', 'edd_wysija'),
+				'desc' => __('Select the list you wish to subscribe buyers to', 'edd_wysija'),
+				'type' => 'select',
+				'options' => $this->get_lists()
+			),
+			array(
+				'id' => 'edd_wysija_label',
+				'name' => __('Checkout Label', 'edd_wysija'),
+				'desc' => __('This is the text shown next to the signup option', 'edd_wysija'),
+				'type' => 'text',
+				'size' => 'regular'
+			),
+			array(
+				'id'      => 'edd_wysija_show_checkout_signup',
+				'name'    => __( 'Show Signup on Checkout', 'edd_wysija' ),
+				'desc'    => __( 'Allow customers to signup for the list selected below during checkout?', 'edd_wysija' ),
+				'type'    => 'checkbox'
+			),
+		);
+
+		return array_merge( $settings, $mailpoet_settings );
+	}
+
+	/**
+	 * Determines if the checkout signup option should be displayed
+	 */
+	public function show_checkout_signup() {
+		global $edd_options;
+
+		return ! empty( $edd_options['edd_wysija_show_checkout_signup'] );
+	}
+
+	/**
+	 * Subscribe an email to a list
+	 */
+	public function subscribe_email( $user_info = array(), $list_id = false, $opt_in_overridde = false ) {
+
+		global $edd_options;
+
+		if( ! class_exists( 'WYSIJA' ) ) {
+			return false;
+		}
+
+		// Retrieve the global list ID if none is provided
+		if( ! $list_id ) {
+			$list_id = ! empty( $edd_options['edd_wysija_list'] ) ? $edd_options['edd_wysija_list'] : false;
+			if( ! $list_id ) {
+				return false;
+			}
+		}
+
+		$user_data = array(
+			'email'     => $user_info['email'],
+			'firstname' => $user_info['first_name'],
+			'lastname'  => $user_info['last_name'],
+		);
+
+		$data = array(
+			'user'=> $user_data,
+			'user_list' => array( 'list_ids' => array( $list_id ) )
+		);
+
+		$userHelper = WYSIJA::get( 'user','helper' );
+		$userHelper->addSubscriber( $data );
+
+		if( $userHelper ) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+}
